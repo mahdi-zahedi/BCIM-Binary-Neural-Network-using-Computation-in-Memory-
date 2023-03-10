f1e4241ca360526223d937ae080be9bee64c8f8b
Mahdi Zahedi <42574243+mahdi-zahedi@users.noreply.github.com>
2023-03-10 18:07:56 +0100

    Add files via upload

--- XNOR-Net-PyTorch/CIFAR_10/models/__init__.py
+++ XNOR-Net-PyTorch/CIFAR_10/models/__init__.py
@@ -1,2 +1,4 @@
 from .nin_non_seq       import *
 from .nin_without_bn       import *
+from .AlexNet       import *
+from .AlexNet_BCIM       import *
--- XNOR-Net-PyTorch/CIFAR_10/util.py
+++ XNOR-Net-PyTorch/CIFAR_10/util.py
@@ -56,7 +56,7 @@ class BinOp():
             m = self.target_modules[index].data.norm(1, 3, keepdim=True)\
                     .sum(2, keepdim=True).sum(1, keepdim=True).div(n)
             self.target_modules[index].data = \
-                    self.target_modules[index].data.sign().mul(m.expand(s))
+                    self.target_modules[index].data.sign()#.mul(m.expand(s))
 
     def restore(self):
         for index in range(self.num_of_params):
